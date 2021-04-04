3b7c1a781ec76612eec2ebab8738168dc80e2de4
Markus <uncle_kracker_1@web.de>
2021-04-04 14:42:50 +1200

    First step in guestbook

--- app.js
+++ app.js
@@ -1,44 +1,57 @@
 "use strict";
 
-// Lecture 1
-// let Car = require("./Car.js");
-
-// let a = new Car();
-// a.drive();
-
-// // Lecture 2
-// let http = require("http");
-
-// let server = http.createServer((request, response) => {
-//   let url = request.url;
-//   if (url == "/mars.html") {
-//     response.write("Hallo Mars!");
-//   } else if (url == "/mond.html") {
-//     response.write("Hallo Mond!");
-//   } else {
-//     response.write("Hallo Welt!");
-//   }
-//   response.end();
-// });
-
-// server.listen(5000);
-
-// Lecture 3
 let express = require("express");
+let fs = require("fs");
+let GuestBookEntry = require("./src/GuestBookEntry");
 
-let app = express();
-app.set("view engine", "ejs");
-app.set("views", "./views");
+fs.readFile("./data.json", "utf-8", (err, data) => {
+  if (err) throw err;
+  let d = JSON.parse(data);
 
-app.get("/index", (req, res) => {
-  res.render("index", { title: "Hallo" });
-});
+  let entries = [];
+  for (let entry of d) {
+    entries.push(new GuestBookEntry(entry.title, entry.content));
+  }
 
-app.get("/index/:id", (req, res) => {
-  let id = req.params.id;
-  res.render("index", { title: id });
-});
+  //Start server
+  let app = express();
+  app.set("view engine", "ejs");
+  app.set("views", "./views");
 
-app.listen(5000, () => {
-  console.log("App wurde gestartet auf localhost:5000");
+  app.use(express.urlencoded({ extended: true }));
+  app.use(express.static("./public"));
+
+  app.get("/index", (req, res) => {
+    res.render("index", { entries: entries });
+  });
+
+  app.get("/index/:id", (req, res) => {
+    let id = req.params.id;
+    res.render("index", { title: id });
+  });
+
+  app.post("/guestbook/new", (req, res) => {
+    let body = req.body;
+    let content = body.content;
+    let title = body.title;
+    entries.push(new GuestBookEntry(title, content));
+    //write new data in data.json
+    fs.writeFile(
+      "./data.json",
+      JSON.stringify(entries),
+      { encoding: "utf-8" },
+      (err) => {
+        if (err) {
+          console.log(err);
+          throw err;
+        }
+      }
+    );
+    res.redirect("/index");
+    res.end();
+  });
+
+  app.listen(5000, () => {
+    console.log("App wurde gestartet auf localhost:5000");
+  });
 });
